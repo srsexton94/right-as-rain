3ddc4bc680c4fbdcbbc654957704d40386b512bb
srsexton94 <srsexton94@gmail.com>
2020-05-14 12:24:31 -0400

    Included proxy in apiUrls - going to change APIs

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 const api = {
-  locationUrl: 'http://dataservice.accuweather.com/locations/v1/cities/search',
-  forecastUrl: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+  locationUrl: 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search',
+  forecastUrl: 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
   key: 'DB1K5R9gBkN13elANKRriWkvoeVs36JG'
 }
 
