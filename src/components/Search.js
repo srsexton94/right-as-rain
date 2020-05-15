f8f18c0b544096a94bbd26cf62223c48e466fdd0
srsexton94 <srsexton94@gmail.com>
2020-05-15 10:49:42 -0400

    Style search bar
    Add styled component to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,16 @@
 import React, {Component, useState} from 'react'
 import {Form, Button, ToggleButtonGroup, ToggleButton} from 'react-bootstrap'
+import styled from 'styled-components'
+
+const FormWrapper = styled.section`
+  border: 1px dotted grey;
+  border-radius: 25%;
+  margin: 2vh auto;
+  max-width: 400px;
+  padding: 2vh 7vh;
+  text-align: center;
+  width: 75vw;
+`
 
 function ToggleUnitChoice() {
   const [value, setValue] = useState('fahrenheit')
@@ -17,11 +28,13 @@ function ToggleUnitChoice() {
 class Search extends Component {
   render () {
     return (
-      <Form onSubmit={this.props.getForecast}>
-        <Form.Control type="text" placeholder="City Name" />
-        <ToggleUnitChoice />
-        <Button variant="primary" type="submit">Get Weather</Button>
-      </Form>
+      <FormWrapper>
+        <Form onSubmit={this.props.getForecast}>
+          <Form.Control type="text" placeholder="City Name" />
+          <ToggleUnitChoice />
+          <Button variant="outline-primary" type="submit">Get Weather</Button>
+        </Form>
+      </FormWrapper>
     )
   }
 }
